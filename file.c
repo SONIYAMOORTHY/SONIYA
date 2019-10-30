d70899b351e0bbd7b54d256b239f084f2acd0c5e
SONIYAMOORTHY <41434998+SONIYAMOORTHY@users.noreply.github.com>
2019-10-30 19:35:32 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n,a[1000],i,j,t;
+scanf("%d ",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d ",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]<a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+printf("The second biggest element of an array is "%d",a[1]);
+
+}
